b912a0502c00322f6457064a37a548ab45c99b5b
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2014-09-10 20:37:24 -0300

    Fazendo uns comentários

--- AgendaGCS/src/model/Contato.java
+++ AgendaGCS/src/model/Contato.java
@@ -16,25 +16,37 @@ public class Contato {
 		super();
 	}
 
+	// Gets the name of the contact.
+
 	public String getNome() {
-		return nome;
+		return nome;		
 	}
+
+	// Sets the name of the contact.
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+	// Gets the telephone of the contact.
+
 	public int getTelefone() {
 		return telefone;
 	}
+
+	// Sets the telephone of the contact.
 
 	public void setTelefone(int telefone) {
 		this.telefone = telefone;
 	}
 
+	// Gets the email of the contact.
+
 	public String getEmail() {
 		return email;
 	}
+
+	// Sets the email of the contact.
 
 	public void setEmail(String email) {
 		this.email = email;
